9c784c17dc55d1e6becfd623aedc3ad5ffb5cd7b
Anders <Anders@ANDERS_PC>
2016-11-15 14:51:21 +0100

    Add project files.

---
+++ Universal TCP Client/ConnectionHandler.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Universal_TCP_Client
+{
+    class ConnectionHandler
+    {
+        private NetworkStream clientStream;
+        public string messege { get; set; } = "kør";
+        public string Name { get; set; }
+
+        public ConnectionHandler(NetworkStream ClientStream, string name)
+        {
+            clientStream = ClientStream;
+            Name = name;
+        }
+
+
+
+        public void Reshive()
+        {
+            while (!messege.ToLower().Equals("stop"))
+            {
+                try
+                {
+                    StreamReader clientStreamReader = new StreamReader(clientStream);
+
+                    Console.WriteLine(clientStreamReader.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    break;
+                }
+
+            }
+
+
+
+        }
+
+        public void Sendmessege()
+        {
+            while (!messege.ToLower().Equals("stop"))
+            {
+                {
+                    try
+                    {
+                        StreamWriter clientStreamWriter = new StreamWriter(clientStream);
+                        clientStreamWriter.AutoFlush = true;
+
+                        messege = Name + ": " + Console.ReadLine();
+                        clientStreamWriter.WriteLine(messege);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        break;
+                    }
+                }
+
+            }
+
+
+        }
+
+    }
+}
---
+++ Universal TCP Client/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Universal_TCP_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter name");
+            string name = Console.ReadLine();
+            Console.WriteLine("press 1 for client and 2 for server");
+            ConsoleKey pressedkey = Console.ReadKey(true).Key;
+
+
+            if (pressedkey.Equals(ConsoleKey.D1) || pressedkey.Equals(ConsoleKey.NumPad1))
+            {
+                try
+                {
+                    Console.WriteLine("please enter ip");
+                    string ip = Console.ReadLine();
+                    string testtesttest = "messege";
+
+                    TcpClient client = new TcpClient(ip, 6789);
+                    var clientStream = client.GetStream();
+                    ConnectionHandler handler = new ConnectionHandler(clientStream, name);
+                    Thread Reshivethread = new Thread(handler.Reshive);
+                    Thread Sendthread = new Thread(handler.Sendmessege);
+                    Reshivethread.Start();
+                    Sendthread.Start();
+                    Console.WriteLine("Du kan nu skrive");
+
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e);
+                }
+            }
+
+            else if (pressedkey.Equals(ConsoleKey.D2) || pressedkey.Equals(ConsoleKey.NumPad2))
+            {
+
+
+                TcpListener server = new TcpListener(IPAddress.Any, 6789);
+                server.Start();
+                TcpClient serverclient = server.AcceptTcpClient();
+                Console.WriteLine("client forbundet");
+                ConnectionHandler handler = new ConnectionHandler(serverclient.GetStream(), name);
+                Thread Reshivethread = new Thread(handler.Reshive);
+                Thread Sendthread = new Thread(handler.Sendmessege);
+                Reshivethread.Start();
+                Sendthread.Start();
+                Console.WriteLine("Du kan nu skrive");
+            }
+            else
+            {
+                Console.WriteLine("button not regconiced");
+            }
+        }
+
+
+
+    }
+}
